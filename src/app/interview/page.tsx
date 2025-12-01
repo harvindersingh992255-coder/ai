84821e88e6fc4429611b2d55e4fdab8748f89bf8
harvindersingh992255-coder <harvindersingh992255@gmail.com>
2025-12-02 02:19:42 +0530

    Create ActiveInterview component for interview page

---
+++ src/app/interview/page.tsx
@@ -0,0 +1,10 @@
+"use client";
+
+export default function ActiveInterview() {
+  return (
+    <div style={{ padding: 30 }}>
+      <h1>Interview Started!</h1>
+      <p>Questions will load here…</p>
+    </div>
+  );
+}
